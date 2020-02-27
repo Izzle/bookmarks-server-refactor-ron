7885f7d3885db8e015850e891a2c1db8a70b86de
Ron Martin <ronmartin89@gmail.com>
2020-02-27 15:59:17 -0800

    have the functional stubs for my tests in place

--- test/bookmarks-endpoints.spec.js
+++ test/bookmarks-endpoints.spec.js
@@ -1,3 +1,34 @@
 const knex = require('knex');
 const app = require('../src/app');
 const { makeBookmarksArray } = require('./bookmarks.fixture');
+
+describe.only('Bookmarks Endpoints', function() {
+  let db;
+
+  before('make knex instance', () => {
+    db = knex({
+      client: 'pg',
+      connection: process.env.TEST_DB_URL
+    });
+    app.set('db', db); // Tests skip server.js, but our app instance expects there to be a 'db' instance
+  });
+
+  after('disconnect from db', () => db.destroy());
+
+  before('clean the table', () => db('bookmarks_table').truncate());
+
+  afterEach('cleanup', () => db('bookmarks_table').truncate());
+
+  describe('GET /bookmarks', () => {
+    context('Given no bookmarks', () => {});
+
+    context('Given there are are bookmarks in the database', () => {});
+  });
+
+  describe('GET /bookmarks/:id', () => {
+    context('Given no bookmarks', () => {});
+
+    context('Given there are are bookmarks in the database', () => {});
+  });
+
+});
