dc2b85f30b7ef770b420242959c2d1ba94ccf8cc
Ron Martin <ronmartin89@gmail.com>
2020-02-27 16:23:17 -0800

    We have working tests for unauthorized requests and no bookmarks

--- test/bookmarks-endpoints.spec.js
+++ test/bookmarks-endpoints.spec.js
@@ -1,6 +1,7 @@
 const knex = require('knex');
 const app = require('../src/app');
 const { makeBookmarksArray } = require('./bookmarks.fixture');
+const store = require('../src/store');
 
 describe.only('Bookmarks Endpoints', function() {
   let db;
@@ -20,8 +21,8 @@ describe.only('Bookmarks Endpoints', function() {
   afterEach('cleanup', () => db('bookmarks_table').truncate());
 
   describe('GET /bookmarks', () => {
-    context('Given an authorized request', () => {
-      it('responds 401 Unauthorized', () => {
+    context('Given an Unauthorized request', () => {
+      it('responds 401 and Unauthorized request', () => {
         return supertest(app)
           .get('/bookmarks')
           .expect(401, { error: 'Unauthorized request' });
@@ -29,7 +30,7 @@ describe.only('Bookmarks Endpoints', function() {
     });
 
     context('Given no bookmarks', () => {
-      it('respoonds 200 and an empty list', () => {
+      it('responds 200 and an empty list', () => {
         return supertest(app)
           .get('/bookmarks')
           .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
@@ -37,10 +38,30 @@ describe.only('Bookmarks Endpoints', function() {
       });
     });
 
-    context('Given there are are bookmarks in the database', () => {});
+    context('Given there are are bookmarks in the database', () => {
+      const testBookmarks = makeBookmarksArray();
+
+      beforeEach('insert bookmarks', () => {
+        return db
+          .into('bookmarks_table')
+          .insert(testBookmarks);
+      });
+
+
+    });
   });
 
   describe('GET /bookmarks/:id', () => {
+    context('Given an Unauthorized request', () => {
+      it('responds 401 and Unauthorized request', () => {
+        // this works now because there is some data in our store, but after we update our POST it may not work anymore
+        const thirdBookmark = store.bookmarks[2]; 
+        return supertest(app)
+          .get(`/bookmarks/${thirdBookmark}`)
+          .expect(401, { error: 'Unauthorized request' });
+      });
+    });
+
     context('Given no bookmarks', () => {});
 
     context('Given there are are bookmarks in the database', () => {});
